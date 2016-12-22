bdb0d7f34510b0605087f5b00e29ef095a0001cb
talum <tracy@flatironschool.com>
2016-12-22 17:08:33 -0500

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+require 'pry'
+
+URL = "https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=40"
+
+response = open(URL).read
+books = JSON.parse(response)
+
+books['items'].each do |item|
+    book = Book.new
+    book.title = item["volumeInfo"]["title"]
+    book.description = item["volumeInfo"]["description"]
+    author = item['volumeInfo']['authors'].first
+    first_name = author.split(" ").first
+    last_name = author.split(" ").last
+    book.authors << Author.find_or_create_by(
+                  first_name: first_name,
+                  last_name: last_name)
+
+    book.save
+end
