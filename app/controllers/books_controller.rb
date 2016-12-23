d5c20360fb7f0b72e2e5634a94f584330a03b2fb
talum <tracy.a.lum@gmail.com>
2016-12-22 21:11:29 -0500

    add edit and update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,16 @@ class BooksController < ApplicationController
     redirect_to @book
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
   private
 
   def book_params
